4012c97e50e3d473bbdae01c4f45740038e782b8
Hugo Habbema <habbema@gmail.com>
2023-09-18 01:11:17 -0300

    teste

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print('Hugo'.upper())
+import os
+#comentário de uma linha
+"""Comentário de várias linhas"""
+
+msg ="Hello world"
+
+linguagem_atual = os.getenv("LANG")[:5]
+print(linguagem_atual)
+
+if linguagem_atual == "pt_BR":
+    msg = "Olá mundo"
+else:
+    msg = "Hello world"
+print(msg)
